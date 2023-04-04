6441a289253a0255880076adb30c40b26037c4bc
Ashay Morwale <ashay123.m@gmail.com>
2023-04-03 22:13:38 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    const temp = [];
+    for (let x = 0; x < names.length; x++) {
+        temp.push(`Thank you, ${names[x]}, for the wonderful ${event} gift!`);
+    }
+    return temp;
+}
+
+function countDown(num) {
+    for (let x = num; x >= 0; x--) {
+        console.log(x)
+    }
+}
